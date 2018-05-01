d0944d945f4f59e6bf0c611bd0eaa73148592d5a
renjm <1149104294@qq.com>
2018-05-01 13:15:11 +0800

    koa-vue api test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios'
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://66.98.120.206:3000',
+  headers: {
+    'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'x-requested-with'
+  }
+});
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
